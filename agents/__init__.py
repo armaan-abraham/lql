60eda1025bcb5d281d0461eb2565c631c20c7cbd
Armaan Abraham <armaanabraham@g.ucla.edu>
2026-02-03 08:06:44 -0800

    remove acrlpd

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,9 +1,7 @@
 from agents.acfql import ACFQLAgent
-from agents.acrlpd import ACRLPDAgent
 from agents.lql import LQLAgent
 
 agents = dict(
     acfql=ACFQLAgent,
-    acrlpd=ACRLPDAgent,
     lql=LQLAgent,
 )
